10e15d49e3351882e2f2e92ffdc1a960a51948b2
bya2 <byaa1972@gmail.com>
2022-04-06 19:08:42 +0900

    컴포넌트 진입점 및 레이아웃

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,23 @@
 import styles from "@styles-components/App.module.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "@pages/home";
-import IDPage from "@pages/id";
 
-export default function App() {
+import Header from "@components/layouts/Header";
+import Main from "@components/layouts/Main";
+import Footer from "@components/layouts/Footer";
+import Modal from "@components/reusable/Modal";
+
+export default function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <div className={styles.App}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path=":id/*" element={<IDPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className={styles.App}>
+      {/* <Header /> */}
+      <Main />
+      {/* <Footer /> */}
+      {/* {
+        (
+          <Modal prop__is_active__bool={true} prop__fn_set__close_modal={() => {}}>
+            <span>OK</span>
+          </Modal>
+        ) as JSX.Element
+      } */}
+    </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "@src/store";
+import { store } from "@/store";
 import App from "@components/App";
 
 ReactDOM.render(
