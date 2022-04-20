da882e8d49a438e2eae1bf0f4a8aaf3a728026fc
bya2 <byaa1972@gmail.com>
2022-04-21 00:00:52 +0900

    Add: Prop 추가

--- client/src/components/reusable/box/Thumbnail.tsx
+++ client/src/components/reusable/box/Thumbnail.tsx
@@ -1,10 +1,10 @@
 import Img from "@/components/reusable/box/Image";
 import type { Props } from "@/models/Props";
 
-export default function Thumb({ cssModule, className }: Props) {
+export default function Thumb({ cssModule, className, prop__element }: Props) {
   return (
     <div className={`${cssModule?.box} ${cssModule?.thumb} ${className}`}>
-      <Img cssModule={cssModule} className={cssModule?.thumb} prop__element={<></>} />
+      <Img cssModule={cssModule} className={cssModule?.thumb} prop__element={prop__element} />
     </div>
   );
 }
