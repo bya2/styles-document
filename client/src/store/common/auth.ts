022dab7e3c3e791fbed18c1e4610437ba9be2120
bya2 <byaa1972@gmail.com>
2022-04-25 19:30:29 +0900

    Auth 전역 상태: 로그인한 사용자 참조, 인증 상태 클릭 상태맵

--- client/src/store/common/auth.ts
+++ client/src/store/common/auth.ts
@@ -1,33 +1,56 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "@/store"
+import type { RootState } from "@/store";
 
-type TAuthRef = {
+import { status_items__at_sign_in__arr, status_items__at_sign_out__arr } from "@/components/common/auth/items";
+import { fn_get__init_s__bool_map } from "@/logic/reusable";
+import type { I_cond_map } from "@/models/reusables";
+
+type T_auth_ref = {
   id: string | null;
   hashed: string | null;
 };
 
-interface IAuthState {
-  ref: TAuthRef
+interface I_auth_state {
+  ref: T_auth_ref;
+  status: {
+    is_click_item__map: I_cond_map;
+  };
 }
 
+const init_s__status__cond_map = fn_get__init_s__bool_map([...status_items__at_sign_in__arr, ...status_items__at_sign_out__arr]);
+
 const name = "authRef";
-const initialState: IAuthState = {
+const initialState: I_auth_state = {
   ref: {
     id: null,
     hashed: null,
-  }
-}
+  },
+  status: {
+    is_click_item__map: init_s__status__cond_map,
+  },
+};
 
-export const authSlice = createSlice({
+export const Slice = createSlice({
   name,
   initialState,
   reducers: {
-    assignRef: (state, action: PayloadAction<TAuthRef>): void => {
+    assignRef: (state, action: PayloadAction<T_auth_ref>): void => {
       state.ref = action.payload;
-    }
-  }
-})
+    },
 
-export const { assignRef } = authSlice.actions;
+    set_s__auth_status__is_click__map: (state, action: PayloadAction<string>) => {
+      state.status.is_click_item__map = {
+        ...state.status.is_click_item__map,
+        [action.payload]: true,
+      };
+    },
+
+    init_s__auth_status__is_click__map: (state) => {
+      state.status.is_click_item__map = init_s__status__cond_map;
+    },
+  },
+});
+
+export const { assignRef } = Slice.actions;
 export const selectAuthRef = (state: RootState) => state.auth.ref;
-export default authSlice.reducer;
+export default Slice.reducer;
