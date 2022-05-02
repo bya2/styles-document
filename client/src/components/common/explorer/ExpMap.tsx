ec65e6eb82290fc9a2bb4df3bd197e3803be7e9d
bya2 <byaa1972@gmail.com>
2022-05-02 17:25:47 +0900

    fix: 탐색기 노드들이 잘못된 루트에 할당되는 문제 해결

--- client/src/components/common/explorer/ExpMap.tsx
+++ client/src/components/common/explorer/ExpMap.tsx
@@ -1,19 +1,14 @@
 import styles from "@styles-components/Explorer.module.scss";
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useEffect, useCallback } from "react";
 import { useAppSelector, useAppDispatch } from "@store/hooks";
 import {
-  set_s__exp__init_mount,
-  // set_s__exp_r_nodes,
   set_s__exp_r_nodes__arr,
-  set_s__exp_r_nodes__is_active__cond_map,
-  set_s__exp_r_node__is_active,
   set_s__exp_nodes__is_active__cond_map,
   set_s__exp_f_nodes__is_fold__cond_map,
   set_s__exp_trees__arr,
   set_s__exp_menu_item__open_modal,
   set_s__exp_menu_item__close_modal,
   set_s__exp_nodes__init_cond_map,
-  add_s__exp_r_node__obj,
   set_s__exp_nodes__arr,
   set_s__exp_nodes__is_loaded,
 } from "@/store/common/explorer";
@@ -27,129 +22,63 @@ import TreeBar from "@/components/common/explorer/TreeBar";
 import ExpRootBar from "@/components/common/explorer/RootBar";
 import Loader from "@/components/reusable/complete/Loader";
 import { layout_menu_items__arr, messages__no_item__arr } from "./items";
-import { fn_get__cond_map_of_uids, fn_get__init_s__bool_map, fn_handle__error__ctx } from "@/logic/reusable";
+import { fn_get__cond_map_of_uids, fn_handle__error } from "@/logic/reusable";
 import {
-  fn_get__nodes_of_roots__arr,
+  fn_get__nodes_of_r_node_uids__arr,
   fn_get__nodes_by_r_node_uid__map,
   fn_get__nodes_by_p_node_uid__map,
   fn_get__layered_nodes__arr,
   fn_get__nodes__cond_map,
-  fn_get__r_node_of_non_children,
+  fn_get__r_nodes_of_non_children,
 } from "@/logic/explorer";
-import type { I_cond_map, I_map } from "@/models/reusables";
-import type { I_exp_node, I_exp_r_node } from "@/models/explorer";
 import { FOLDER_TYPE, ROOT_TYPE } from "@/config/explorer";
-
-const init_s__is_click__map: I_map<boolean> = fn_get__init_s__bool_map([...layout_menu_items__arr]);
+import { useParams } from "react-router-dom";
 
 export default function Explorer(): JSX.Element {
-  // State
-  const s__act_roots = useAppSelector((s) => s.activity.r_nodes);
-  const s__act_roots__is_mouse_down_root__map = s__act_roots.is_mouse_down__cond_map;
+  const { userId: param__user_id } = useParams();
 
-  const s__explorer = useAppSelector((s) => s.explorer);
-  const s__exp_nodes__arr = s__explorer.nodes.arr;
-  const s__exp_nodes__is_loaded = s__explorer.nodes.is_loaded;
-  const s__exp__is_init_mount = s__explorer.is_mounted;
-  const s__exp_r_nodes__arr = s__explorer.r_nodes.arr;
-  const s__exp_nodes__init_s__cond_map = s__explorer.nodes.init_s__cond_map;
-  const s__exp_nodes__is_active__cond_map = s__explorer.nodes.is_active__cond_map;
-  const s__exp_f_nodes__is_fold__cond_map = s__explorer.nodes.is_fold__cond_map;
-  const s__exp_menus__is_click__cond_map = s__explorer.menus.is_click__cond_map;
-  const s__exp_trees__arr = s__explorer.trees.arr;
-  // const s__auth = useAppSelector((s) => s.auth);
+  const s__exp = useAppSelector((s) => s.explorer);
+  const s__exp_nodes__arr = s__exp.nodes.arr;
+  const s__exp_nodes__is_loaded = s__exp.nodes.is_loaded;
+  const s__exp_r_nodes__arr = s__exp.r_nodes.arr;
+  const s__exp_menus__is_click__cond_map = s__exp.menus.is_click__cond_map;
+  const s__exp_trees__arr = s__exp.trees.arr;
   const dispatch = useAppDispatch();
 
-  const [s__is_mouse_enter__map, set_s__is_mouse_enter__map] = useState<I_map<boolean>>({ bar__tree: false });
-  const [s__is_click__map, set_s__is_click__map] = useState<I_map<boolean>>(init_s__is_click__map);
-
-  // Handler
-  // const cb_handle__mouse_enter__exp2_area: T_Handler<React.MouseEvent> = useCallback(
-  //   (e) => {
-  //     const dragged: string | undefined = Object.keys(s__act_roots__is_mouse_down_root__map).find(
-  //       (key) => s__act_roots__is_mouse_down_root__map[key] === true
-  //     );
-  //     console.log("DRAGG:", dragged);
-  //     if (!dragged) return;
-
-  //     const is_duplicated = s__exp_r_nodes__arr.find((el) => el?.uid === dragged);
-  //     console.log("DUPLICATE:", is_duplicated);
-  //     if (is_duplicated) return;
-
-  //     set_s__is_mouse_enter__map({
-  //       ...s__is_mouse_enter__map,
-  //       bar__tree: true,
-  //     });
-
-  //     e.currentTarget.addEventListener(
-  //       "mouseleave",
-  //       () => {
-  //         set_s__is_mouse_enter__map({
-  //           ...s__is_mouse_enter__map,
-  //           bar__tree: false,
-  //         });
-  //       },
-  //       { once: true }
-  //     );
-
-  //     e.currentTarget.addEventListener(
-  //       "mouseup",
-  //       () => {
-  //         const exp_root__obj: I_exp_r_node = { uid: dragged, type:  };
-
-  //         dispatch(add_s__exp_r_node__obj(exp_root__obj);
-
-  //         set_s__is_mouse_enter__map({
-  //           ...s__is_mouse_enter__map,
-  //           bar__tree: false,
-  //         });
-  //       },
-  //       { once: true }
-  //     );
-  //   },
-  //   [dispatch, s__act_roots__is_mouse_down_root__map, s__exp_r_nodes__arr, s__is_mouse_enter__map]
-  // );
-
-  const cb_get__nodes_of_roots__arr = useCallback(fn_get__nodes_of_roots__arr, []);
+  const cb_get__nodes_of_roots__arr = useCallback(fn_get__nodes_of_r_node_uids__arr, []);
   const cb_get__nodes_by_r_node_uid__map = useCallback(fn_get__nodes_by_r_node_uid__map, []);
   const cb_get__nodes_by_p_node_uid__map = useCallback(fn_get__nodes_by_p_node_uid__map, []);
   const cb_get__layered_nodes__arr = useCallback(fn_get__layered_nodes__arr, []);
   const cb_get__nodes__cond_map = useCallback(fn_get__nodes__cond_map, []);
 
-  // SIDE - Update, Non-Loaded
-  useEffect(() => {
-    if (s__exp_nodes__is_loaded) return;
+  const cb_handle__side_update__at_not_loaded_nodes = useCallback(() => {
     if (s__exp_r_nodes__arr.length === 0) return;
 
     const r_node_uids__arr = s__exp_r_nodes__arr.reduce((arr: string[], r_node) => [...arr, r_node.uid], []);
 
     cb_get__nodes_of_roots__arr(r_node_uids__arr)
-      .then((nodes__arr: I_exp_node[]) => {
-        dispatch(set_s__exp_nodes__arr(nodes__arr));
+      .then((nodes_of_roots__arr) => {
+        dispatch(set_s__exp_nodes__arr(nodes_of_roots__arr));
         dispatch(set_s__exp_nodes__is_loaded(true));
       })
-      .catch((err) => fn_handle__error__ctx(err, "LOC: ExpMap"));
-  }, [s__exp_nodes__is_loaded, s__exp_r_nodes__arr, dispatch, cb_get__nodes_of_roots__arr]);
+      .catch((err) => {
+        fn_handle__error(err, {
+          loc: "ExpMap.side.update.at_not_loaded_nodes",
+        });
+      });
+  }, [s__exp_r_nodes__arr, dispatch, cb_get__nodes_of_roots__arr]);
 
-  // SIDE - Update, Loaded
-  useEffect(() => {
-    if (!s__exp_nodes__is_loaded) return;
-
-    console.log("fdsa", s__exp_nodes__arr);
-
+  const cb_handle__side_update__at_loaded_nodes = useCallback(() => {
     const r_node_uids__arr = s__exp_r_nodes__arr.reduce((arr: string[], r_node) => [...arr, r_node.uid], []);
 
-    let trees__arr = fn_get__r_node_of_non_children(r_node_uids__arr);
+    let trees__arr = fn_get__r_nodes_of_non_children(r_node_uids__arr);
     let nodes__cond_map = fn_get__cond_map_of_uids(r_node_uids__arr);
     let f_nodes__cond_map = fn_get__cond_map_of_uids(r_node_uids__arr);
 
     const nodes_by_r_node_uid__map = cb_get__nodes_by_r_node_uid__map(s__exp_nodes__arr);
 
-    let r_node_idx = 0;
     for (const [r_node_uid, nodes_by_r_node_uid__arr] of Object.entries(nodes_by_r_node_uid__map)) {
       const nodes_by_p_node_uid__map = cb_get__nodes_by_p_node_uid__map(nodes_by_r_node_uid__arr);
-
-      console.log("mmmm", nodes_by_p_node_uid__map);
 
       const layered_nodes__arr = cb_get__layered_nodes__arr({
         _cb: cb_get__layered_nodes__arr,
@@ -157,7 +86,10 @@ export default function Explorer(): JSX.Element {
         p_node_uid: r_node_uid,
       });
 
-      trees__arr[r_node_idx].children = layered_nodes__arr;
+      const r_node__obj = trees__arr.find((r_node) => r_node.uid === r_node_uid);
+      if (r_node__obj) {
+        r_node__obj.children = layered_nodes__arr;
+      }
 
       nodes__cond_map = {
         ...nodes__cond_map,
@@ -168,8 +100,6 @@ export default function Explorer(): JSX.Element {
         ...f_nodes__cond_map,
         ...cb_get__nodes__cond_map(nodes_by_r_node_uid__arr, FOLDER_TYPE),
       };
-
-      r_node_idx++;
     }
 
     dispatch(set_s__exp_trees__arr(trees__arr));
@@ -177,7 +107,6 @@ export default function Explorer(): JSX.Element {
     dispatch(set_s__exp_nodes__is_active__cond_map(nodes__cond_map));
     dispatch(set_s__exp_f_nodes__is_fold__cond_map(f_nodes__cond_map));
   }, [
-    s__exp_nodes__is_loaded,
     s__exp_r_nodes__arr,
     s__exp_nodes__arr,
     dispatch,
@@ -187,18 +116,31 @@ export default function Explorer(): JSX.Element {
     cb_get__nodes__cond_map,
   ]);
 
-  // Side: Update
   useEffect(() => {
-    if (s__is_click__map[layout_menu_items__arr[0].id]) {
-      // Exp2 분할 및 루트 추가
+    if (param__user_id) {
+      dispatch(
+        set_s__exp_r_nodes__arr([
+          {
+            uid: param__user_id,
+            type: ROOT_TYPE,
+          },
+        ])
+      );
+      dispatch(set_s__exp_nodes__is_loaded(false));
     }
+  }, [dispatch, param__user_id]);
 
-    if (s__is_click__map[layout_menu_items__arr[1].id]) {
-      // Exp 확장
+  useEffect(() => {
+    if (!s__exp_nodes__is_loaded) {
+      cb_handle__side_update__at_not_loaded_nodes();
     }
+  }, [s__exp_nodes__is_loaded, cb_handle__side_update__at_not_loaded_nodes]);
 
-    // set_s__is_click__map(init_s__is_click__map);
-  }, [s__is_click__map]);
+  useEffect(() => {
+    if (s__exp_nodes__is_loaded) {
+      cb_handle__side_update__at_loaded_nodes();
+    }
+  }, [s__exp_nodes__is_loaded, cb_handle__side_update__at_loaded_nodes]);
 
   return (
     <Map cssModule={styles} className={styles.exp}>
@@ -207,7 +149,7 @@ export default function Explorer(): JSX.Element {
         <MenuBar />
       </Region>
 
-      <Region cssModule={styles} className={`${styles.bottom}`} /*onMouseEnter={(e) => cb_handle__mouse_enter__exp2_area(e)} */>
+      <Region cssModule={styles} className={`${styles.bottom}`}>
         {s__exp_trees__arr.length > 0 ? (
           <TreeBar />
         ) : s__exp_r_nodes__arr.length === 0 ? (
@@ -220,8 +162,7 @@ export default function Explorer(): JSX.Element {
             }}
           />
         ) : (
-          <TreeBar />
-          // <Loader cssModule={styles} className={styles.spinner} />
+          <Loader cssModule={styles} className={styles.spinner} />
         )}
       </Region>
 
