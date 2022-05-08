b110a5028af167bcee92b5c67568585bcdbcb5e6
bya2 <byaa1972@gmail.com>
2022-05-08 23:24:48 +0900

    최근 방문한 페이지에 대한 Redux 적용 상태 적용, 코드 간결화

--- client/src/components/common/visit/RecentBar.tsx
+++ client/src/components/common/visit/RecentBar.tsx
@@ -1,26 +1,29 @@
 import styles from "@styles-components/Visit.module.scss";
+import { useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { set_s__act_tool__is_active } from "@/store/common/activity";
 import { Link } from "react-router-dom";
 import UList from "@/components/reusable/bar/UList";
 import Item from "@/components/reusable/group/Item";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import React, { useCallback } from "react";
-import { set_s__act_tool__is_active_item } from "@/store/common/activity";
 
 export default function RecentBar() {
-  const s__visit_items__arr = useAppSelector((s) => s.activity.roots.arr);
+  const s__act_r_nodes__arr = useAppSelector((s) => s.activity.r_nodes.arr);
   const dispatch = useAppDispatch();
 
-  const cb_handle__click__visit_item = useCallback((e: React.MouseEvent<HTMLLIElement>, _id: string) => {
-    e.stopPropagation();
-    dispatch(set_s__act_tool__is_active_item("explorer"));
-  }, [dispatch]);
+  const cb_handle__click__visit_item = useCallback(
+    (e: React.MouseEvent<HTMLLIElement>, _id: string) => {
+      e.stopPropagation();
+      dispatch(set_s__act_tool__is_active({ id: "explorer", cond: true }));
+    },
+    [dispatch]
+  );
 
   return (
     <UList cssModule={styles} className={styles.recent}>
-      {s__visit_items__arr.map((item__obj) => {
+      {s__act_r_nodes__arr.map((item__obj) => {
         const { id, occured_at } = item__obj;
         return (
-          <Item key={id} cssModule={styles} className={styles.recent} onClick={e => cb_handle__click__visit_item(e, id)}>
+          <Item key={id} cssModule={styles} className={styles.recent} onClick={(e) => cb_handle__click__visit_item(e, id)}>
             <Link to={`/${id}`}>
               <span>{id}</span>
               <span>{occured_at}</span>
