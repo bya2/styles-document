b5fbd90937e74e3f277bba9d4547d4c849ff5d3d
bya2 <byaa1972@gmail.com>
2022-05-08 23:31:36 +0900

    fix: UserPage로 이동하지 않는 문제 해결

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "@store/hooks";
+import { local_storage_keys__map } from "@/config/storage";
+import { fn_get__ls_arr_item__arr } from "@/logic/reusable";
 
 export default function HomePage(): JSX.Element {
   const navigate = useNavigate();
@@ -8,10 +10,13 @@ export default function HomePage(): JSX.Element {
   const s__auth__ref__id = useAppSelector((s) => s.auth.ref.id);
 
   useEffect(() => {
-    const cached_ids__str: string | null = window.localStorage.getItem("visited_users");
+    let cached_r_nodes__arr = fn_get__ls_arr_item__arr<I_act_r_node>(local_storage_keys__map.visited_pages);
+    console.log("dfsaf", cached_r_nodes__arr);
 
-    if (cached_ids__str) {
-      const _url: string = s__auth__ref__id ?? cached_ids__str.split(",")[0];
+    if (s__auth__ref__id) {
+      navigate(s__auth__ref__id);
+    } else if (cached_r_nodes__arr) {
+      const _url = cached_r_nodes__arr[0].id;
       navigate(_url, { replace: true });
     }
   }, [navigate, s__auth__ref__id]);
