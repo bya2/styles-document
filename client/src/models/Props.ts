e8c4a3d6157b3e5bf141a8bf3be03b35ad9b4ea7
bya2 <byaa1972@gmail.com>
2022-05-16 10:07:22 +0900

    Mod: 컴포넌트에 대한 Props를 더 간단하게 변경함에 따라 인터페이스 변경 및 추가

--- client/src/models/Props.ts
+++ client/src/models/Props.ts
@@ -5,6 +5,7 @@ import {
   FormHTMLAttributes,
   HTMLAttributes,
   InputHTMLAttributes,
+  LabelHTMLAttributes,
   LiHTMLAttributes,
   TextareaHTMLAttributes,
 } from "react";
@@ -12,6 +13,7 @@ import { I_map } from "./reusables";
 
 export interface I_bs_props {
   cssModule?: I_map<string>;
+  style_obj?: I_map<string>;
   inlineStyle?: React.CSSProperties;
   prop__id?: string;
   prop__title?: string;
@@ -20,6 +22,40 @@ export interface I_bs_props {
   prop__legend?: string;
   prop__volume?: number;
   prop__element?: JSX.Element | React.FC | React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+}
+
+export interface I_re_props {
+  style_obj: I_map<string>;
+  _class?: string;
+  _classes?: string;
+  _image?: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  _alt?: string;
+  _content?: string;
+  _title?: string;
+  _legend?: string;
+  _element?: JSX.Element | React.FC;
+}
+
+export interface I_div_props extends HTMLAttributes<HTMLDivElement> {}
+export interface I_ul_props extends HTMLAttributes<HTMLUListElement> {}
+export interface I_li_props extends LiHTMLAttributes<HTMLLIElement> {}
+export interface I_form_props extends FormHTMLAttributes<HTMLFormElement> {}
+export interface I_btn_props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export interface I_div_attrs extends HTMLAttributes<HTMLDivElement> {}
+export interface I_ul_attrs extends HTMLAttributes<HTMLUListElement> {}
+export interface I_li_attrs extends LiHTMLAttributes<HTMLLIElement> {}
+export interface I_form_attrs extends FormHTMLAttributes<HTMLFormElement> {}
+export interface I_btn_attrs extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface I_label_attrs extends LabelHTMLAttributes<HTMLLabelElement> {}
+export interface I_input_attrs extends InputHTMLAttributes<HTMLInputElement> {}
+
+export interface I_inp_grp_props {
+  _class: string;
+  _id: string;
+  _label: string;
+  label_attrs?: I_label_attrs;
+  input_attrs?: I_input_attrs;
 }
 
 export interface I_div_bs_props extends I_bs_props, HTMLAttributes<HTMLDivElement> {}
