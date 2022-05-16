db77ab32ece90e2609ce113d0021e81369454c92
bya2 <byaa1972@gmail.com>
2022-05-16 10:05:15 +0900

    Doc Elem 요소에 대한 인터페이스

--- client/src/models/document.ts
+++ client/src/models/document.ts
@@ -4,10 +4,16 @@ export interface I_doc_item_ref {
   name: string;
 }
 
-export interface I_doc_item extends I_doc_item_ref {
-
-}
+export interface I_doc_item extends I_doc_item_ref {}
 
 export interface I_doc_img_item extends I_doc_item_ref {
   img: string | HTMLImageElement | SVGImageElement | File;
+}
+
+export interface I_doc__elem {
+  uid: string;
+  doc_uid: string;
+  title: string;
+  content?: string; // TEXT
+  volume?: number;
 }
