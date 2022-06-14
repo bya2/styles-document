c4baa35346e87ba8196e322bbc81b449c9f66bd6
bya2 <byaa1972@gmail.com>
2022-06-14 22:11:54 +0900

    hook: useDir - Actions 분리 및 인터페이스 작성, action 일부 간결화

--- client/src/hooks/useDir.ts
+++ client/src/hooks/useDir.ts
@@ -1,32 +1,38 @@
-import { useState, useCallback } from "react";
-import type { IDir } from "@/@types/reusable";
+import { useState, useMemo } from "react";
 
-export const getDirOfObjList = <V>(_arr: any[], _tgProp: string, _initValue: V): IDir<V> => {
-  return _arr.reduce((_dir: IDir<V>, _obj: any) => {
-    _dir[_obj[_tgProp]] = _initValue;
-    return _dir;
-  }, {});
+export interface Actions<T extends object> {
+  set: (newDir: T) => void;
+  upsert: <K extends keyof T>(key: K, value: T[K]) => void;
+  remove: <K extends keyof T>(key: K) => void;
+}
+
+const useDir = <T extends object = any>(_initDir: T = {} as T): [T, Actions<T>] => {
+  const [dir, set] = useState<T>(_initDir);
+
+  const actions = useMemo<Actions<T>>(
+    () => ({
+      set: (newDir: T) => {
+        set(newDir);
+      },
+      upsert: (key, value) => {
+        set((prevDir) => {
+          return {
+            ...prevDir,
+            [key]: value,
+          };
+        });
+      },
+      remove: (key) => {
+        set((prevDir) => {
+          const { [key]: omit, ...rest } = prevDir;
+          return rest as T;
+        });
+      },
+    }),
+    [] // <-? set
+  );
+
+  return [dir, actions];
 };
 
-export default function useDir<V>(_arr: any[], _tgProp: string, _initValue: V) {
-  const [dir, setDir] = useState<IDir<V>>(() => getDirOfObjList<V>(_arr, _tgProp, _initValue));
-
-  const upsertDir = useCallback((_tgProp: string, _initValue: V) => {
-    setDir((prev) => {
-      return {
-        ...prev,
-        [_tgProp]: _initValue,
-      };
-    });
-  }, []);
-
-  const removeDir = useCallback((_tgProp: string) => {
-    setDir((prev) => {
-      const curr = { ...prev };
-      delete curr[_tgProp];
-      return curr;
-    });
-  }, []);
-
-  return [dir, { upsertDir, removeDir }];
-}
+export default useDir;
